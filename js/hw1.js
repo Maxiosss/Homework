473d32703c9fc457eed9c9b8a3cf44ddb1065164
Maxiosss <129985137+Maxiosss@users.noreply.github.com>
2024-02-10 23:20:50 +0100

    js

--- js/hw1.js
+++ js/hw1.js
@@ -28,3 +28,31 @@ console.log(myNumber);
 const myNull = null;
 
 console.log(myNull);
+
+const userName = prompt("Будь ласка, введіть своє ім'я: ");
+
+if (userName !== null & userName !== "") {
+    alert("Привіт," + userName + "!");
+} 
+else {
+    alert("Ви не ввели своє ім'я!");
+}
+
+const question = confirm("Ви впевнені що хочете підтвердити цю дію?");
+if (question){
+    alert("Дякую за підтвердження!");
+}
+else{
+    alert("Дію відмінено!");
+}
+
+alert("Ця дія є небезпечна тому просимо Вас підтвердити дію");
+
+const confirmation = confirm('Ви бажаєте підтвердити цю дію?');
+
+if (confirmation){
+    alert("Дякую за підтвердження!");
+}
+else{
+    alert("Дію відмінено!");
+}
--- js/hw2.js
+++ js/hw2.js
@@ -1,27 +1,4 @@
-const userName = prompt("Будь ласка, введіть своє ім'я: ");
-
-if (userName !== null & userName !== "") {
-    alert("Привіт," + userName + "!");
-} 
-else {
-    alert("Ви не ввели своє ім'я!");
-}
-
-const question = confirm("Ви впевнені що хочете підтвердити цю дію?");
-if (question){
-    alert("Дякую за підтвердження!");
-}
-else{
-    alert("Дію відмінено!");
-}
-
-alert("Ця дія є небезпечна тому просимо Вас підтвердити дію");
-
-const confirmation = confirm('Ви бажаєте підтвердити цю дію?');
-
-if (confirmation){
-    alert("Дякую за підтвердження!");
-}
-else{
-    alert("Дію відмінено!");
-}
+const minVal = 1.0;
+const maxVal = 10.0;
+const randomNum = Math.random() * (maxVal - minVal) + minVal;
+console.log(randomNum);
