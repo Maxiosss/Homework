532ae4b3192b5e128bba5af7e2e4ec66122f386d
Maxiosss <129985137+Maxiosss@users.noreply.github.com>
2024-02-22 20:38:43 +0100

    hw

---
+++ js/hw3.js
@@ -0,0 +1,20 @@
+const result = 5 + 5 + "5";
+console.log(typeof result);
+console.log(result);
+
+const email = "non.public@gmail.com";
+console.log(email.includes("@"));
+console.log(email.length);
+console.log(email);
+
+const my = "My";
+const name = " name ";
+const is = "is";
+
+const fullName = my + name + is + " Viktor";
+console.log(fullName);
+
+const userName = "Олександро";
+const payment = "300";
+
+alert(`Дякуємо, ${userName}! До сплати ${payment} гривень`);
