2eb4e640f34a20794be242013d499370163a8301
Maxiosss <129985137+Maxiosss@users.noreply.github.com>
2024-02-25 16:59:14 +0100

    hw

---
+++ js/hw4.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const newImg = document.querySelector(".mountains");
+newImg.src = "./img/mountains.jpg";
+
+const link = document.querySelector(".link");
+link.href = "https://images.app.goo.gl/t9a798deDE3hpA2y6";
+
+const newAlt = document.querySelector(".sea");
+newAlt.alt = "sea";
