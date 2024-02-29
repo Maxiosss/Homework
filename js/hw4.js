40138842aba912b7bc5808332834d16fc02515dc
Maxiosss <129985137+Maxiosss@users.noreply.github.com>
2024-02-29 19:31:09 +0100

    hw

--- js/hw4.js
+++ js/hw4.js
@@ -8,3 +8,10 @@ link.href = "https://images.app.goo.gl/t9a798deDE3hpA2y6";
 
 const newAlt = document.querySelector(".sea");
 newAlt.alt = "sea";
+
+const newText = document.querySelector(".button--change");
+const text = document.querySelector(".input--value");
+newText.textContent = text.value;
+
+const newParagraph = document.querySelector(".list__text");
+newParagraph.textContent = 'Element 1';
